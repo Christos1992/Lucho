3ff14fa55a141a8accb58231c2a022aea212f8fe
christos angelopoulos <ch.angelopoulos@hotmail.com>
2018-06-27 18:00:46 +0200

    side effects finaly done

--- app/controllers/side_effect_incidents_controller.rb
+++ app/controllers/side_effect_incidents_controller.rb
@@ -8,6 +8,18 @@ class SideEffectIncidentsController < ApplicationController
    if @side_effect_incident.save
      @allsideeffects = SideEffect.all
 
+    @side_effect_names = []
+    @allsideeffects.each do |sideeffect|
+      if !@side_effect_names.include? "#{sideeffect.name}"
+        @side_effect_names << sideeffect
+      end
+    end
+    @side_effect_names = @side_effect_names.uniq { |p| p.name }
+    @allmedications = current_user.parent.child.child_medications
+    @names =[]
+    @allmedications.each do |medication|
+      @names << medication.medication.name
+    end
       respond_to do |format|
         format.html { redirect_to incident_path(@incident) } #NEED TO FIX THE PATH
         format.js  # <-- will render `app/views/side_effect_incidents/create.js.erb`
@@ -20,10 +32,19 @@ class SideEffectIncidentsController < ApplicationController
     end
   end
 
+  def destroy
+    @side_effect_incident = SideEffectIncident.find(params[:id])
+    @side_effect_incident.destroy
+      respond_to do |format|
+        format.js  # <-- idem
+      end
+  end
+
+
   private
 
   def side_effect_incident_params
-    params.require(:side_effect_incident).permit(:incident_id, :side_effect_id)
+    params.require(:side_effect_incident).permit(:side_effect_id)
   end
 
 end
--- app/models/side_effect_incident.rb
+++ app/models/side_effect_incident.rb
@@ -1,7 +1,4 @@
 class SideEffectIncident < ApplicationRecord
   belongs_to :incident
   belongs_to :side_effect
-
-
-  validates :side_type, inclusion: { in: ['mild', 'sever', 'deqdly']}
 end
