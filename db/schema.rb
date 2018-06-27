73f5d5ca6b0af91e785b215d6e1695211e5e1c5e
Evrard de Visscher <evrarddevisscher@hotmail.com>
2018-06-27 10:42:21 +0200

    installing calendar

--- db/schema.rb
+++ db/schema.rb
@@ -96,9 +96,9 @@ ActiveRecord::Schema.define(version: 2018_06_26_094808) do
   create_table "side_effects", force: :cascade do |t|
     t.string "name"
     t.string "none"
-    t.string "mild"
-    t.string "moderate"
-    t.string "severe"
+    t.string "mid"
+    t.string "modarate"
+    t.string "sever"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
