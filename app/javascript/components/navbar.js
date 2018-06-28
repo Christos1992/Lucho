6f4d0f3c82ba3cf3705513e90efbac2a8fa4e779
Evrard de Visscher <evrarddevisscher@hotmail.com>
2018-06-28 11:22:23 +0200

    transition navbar working great

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,9 +2,10 @@ function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 50) {
         navbar.classList.add('navbar-wagon-white');
       } else {
+        console.log("not working")
         navbar.classList.remove('navbar-wagon-white');
       }
     });
