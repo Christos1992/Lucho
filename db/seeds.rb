265c6674efd8f9018a6a3a367e6a63d2236dba2a
Jonas Morgenthaler <jonas.n.morgenthaler@gmail.com>
2018-06-29 11:14:15 +0200

    seed repaired

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "creating a User"
-User.create!(email: "user@user.com", password: "123456")
+us = User.create!(email: "user@user.com", password: "123456")
 puts "making medications"
 number = 1
 10.times do
@@ -14,11 +14,11 @@ Medication.create!(name: "test#{number}", description: "this drug is very strong
 number = number +1
 end
 puts "creating a parent"
-Parent.create!(first_name: "Christos", last_name: "Angel", gender:"male", date_of_birth: "28/03/1992", working_status: "employed", civil_status:"marrined", hobbies: "football and wine", share_info: true, user_id: 1)
+par = Parent.create!(first_name: "Christos", last_name: "Angel", gender:"male", date_of_birth: "28/03/1992", working_status: "employed", civil_status:"marrined", hobbies: "football and wine", share_info: true, user_id: us.id)
 puts "creating a child"
-Child.create!(first_name: "Jonas", last_name: "Montan", gender: "boy", date_of_birth: "22/03/1983", type_of_cancer: "Leukemia", stage_of_cancer: 1, first_time_patient: true, date_treatment_begin: "11/11/2017", hospital_name: "saint george", doctor_name: "Dr.Love", school: true, biggest_dream: "go to the moon", parent_id: 1)
+chi = Child.create!(first_name: "Jonas", last_name: "Montan", gender: "boy", date_of_birth: "22/03/1983", type_of_cancer: "Leukemia", stage_of_cancer: 1, first_time_patient: true, date_treatment_begin: "11/11/2017", hospital_name: "saint george", doctor_name: "Dr.Love", school: true, biggest_dream: "go to the moon", parent_id: par.id)
 puts "creating childmedication"
-ChildMedication.create!(perscriptioned_doses: 1, medication_id: 1, child_id: 1)
+ChildMedication.create!(perscriptioned_doses: 1, medication_id: Medication.all.sample.id, child_id: chi.id)
 
 #External Radiation Side Effects
 SideEffect.create!(name: "Fatigue", metric: "Mild - Normal activity with effort")
