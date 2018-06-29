17b6229f3bebca3b6b4304e323eadb8a82bef31b
Arnau Izard Torredemer <arnau.izardtorredemer@solvaypostgrad.net>
2018-06-29 12:57:20 +0200

    log out if there is no parent, plus dates fixed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+
+  before_action :authenticate_parent!
+  skip_before_action :authenticate_parent! , if: :devise_controller?
+  def authenticate_parent!
+
+    redirect_to new_parent_path if !current_user.nil? && current_user.parent.nil?
+
+  end
 end
